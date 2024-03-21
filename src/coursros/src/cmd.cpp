84c979f4c6668d4a30e3bb9aa045ec2c39d65887
Hugo1304 <155458755+Hugo1304@users.noreply.github.com>
2024-03-21 12:03:13 +0100

    Add files via upload

--- src/coursros/src/cmd.cpp
+++ src/coursros/src/cmd.cpp
@@ -5,6 +5,15 @@
 #include <sstream>
 #include <geometry_msgs/Twist.h>
 
+const double linear_max_speed = 0.1; // Vitesse maximale linéaire
+const double angular_max_speed = 0.4; // Vitesse maximale angulaire
+const double linear_acceleration = 0.01; // Accélération linéaire
+const double angular_acceleration = 0.05; // Accélération angulaire
+
+double clamp(double value, double min, double max) {
+    return std::min(std::max(value, min), max);
+}
+
 int getch() {
     struct termios oldt, newt;
     int ch;
@@ -31,41 +40,35 @@ int main(int argc, char **argv) {
     ros::init(argc, argv, "cmd");
     ros::NodeHandle nh;
     ros::Publisher cmd_vel_pub = nh.advertise<geometry_msgs::Twist>("cmd_vel", 10);
-    ros::Rate loop_rate(1);
+    ros::Rate loop_rate(10); // Taux de boucle à 10 Hz pour un contrôle plus réactif
 
+    double linear_speed = 0.0; // Vitesse linéaire actuelle
+    double angular_speed = 0.0; // Vitesse angulaire actuelle
 
     while (ros::ok()) {
-       
         geometry_msgs::Twist twist;
+
         int clavier = getch();
 
-         if (clavier == '8') {
-            
-            twist.linear.x = 2.0;
-            twist.angular.z = 0;
-         }  
-                     
-        if (clavier == '2'){
-                twist.linear.x = -2.0;  // Reculer
-                twist.angular.z = 0;
-        }
-              
-        if (clavier == '4'){
-                twist.angular.z = 2.0;  // Tourner à gauche
-                twist.linear.x = 0;
-        }
-               
-        if (clavier == '6'){
-                twist.angular.z = -2.0;  // Tourner à droite
-                twist.linear.x = 0;
+        if (clavier == 'z') {
+            linear_speed = clamp(linear_speed + linear_acceleration, -linear_max_speed, linear_max_speed);
+        } else if (clavier == 's') {
+            linear_speed = clamp(linear_speed - linear_acceleration, -linear_max_speed, linear_max_speed);
+        } else if (clavier == 'q') {
+            angular_speed = clamp(angular_speed + angular_acceleration, -angular_max_speed, angular_max_speed);
+        } else if (clavier == 'd') {
+            angular_speed = clamp(angular_speed - angular_acceleration, -angular_max_speed, angular_max_speed);
+        } else if (clavier == 'a') {
+            linear_speed = 0.0;
+            angular_speed = 0.0;
+        } else if (clavier == 'h') {
+            ROS_INFO("Arret du programme !");
+            ros::shutdown(); // Arrêter le noeud ROS
+            break; // Sortir de la boucle while
         }
 
-        if (clavier == '5'){
-                twist.angular.z = 0;  // Arret
-                twist.linear.x = 0;                       
-        }
-                
-        
+        twist.linear.x = linear_speed;
+        twist.angular.z = angular_speed;
 
         cmd_vel_pub.publish(twist);
         loop_rate.sleep();
